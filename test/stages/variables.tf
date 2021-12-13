79c8d60392618f3ef78a3588a87e993fb1fb9f7e
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2021-12-13 13:25:54 +0530

    cleaned up files
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -1,6 +1,7 @@
 variable "region"{
   type = string
-  default ="ap-south-1"
+  default ="ap-south-1" 
+  description = "Please set the region where the resouces to be created "
 }
 
 variable "access_key"{
