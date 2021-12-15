e24741f8564f6a9aff44a683715f7837171aac11
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2021-12-15 21:27:33 +0530

    updated aws configure
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -4,13 +4,16 @@ echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 export VPC_ID=$(terraform output -json | jq '."dev-vpc-id".value')
 REGION=$(cat terraform.tfvars | grep -E "^region" | sed "s/region=//g" | sed 's/"//g')
 
-export AWS_DEFAULT_REGION=REGION
-#echo "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}"
+export AWS_REGION=${REGION}
+echo "AWS_REGION: ${AWS_REGION}"
+echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
+echo "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}"
+
 
 echo "VPC_ID: ${VPC_ID}"
 echo "REGION: ${REGION}"
 
-aws configure set region ${REGION}
+aws configure #set region ${REGION}
 
 echo "Checking VPC exists with ID in AWS: ${VPC_ID}"
 VPC_ID_OUT=$(aws ec2 describe-vpcs --vpc-ids ${VPC_ID} --query 'Vpcs[0].VpcId' --output=json) 
--- test/stages/stage2-vpc.tf
+++ test/stages/stage2-vpc.tf
@@ -3,4 +3,5 @@ module "dev_vpc" {
   prefix_name=var.prefix_name
   internal_cidr=var.internal_cidr
   instance_tenancy=var.instance_tenancy
+  vpc_id = "vpc-0c5f482e03ec5be11"
 }
