fa2c2492871318ff832f13d979a32099a72e3b92
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2021-12-15 22:21:04 +0530

    updated aws configure
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -4,17 +4,19 @@ echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 export VPC_ID=$(terraform output -json | jq '."dev-vpc-id".value')
 REGION=$(cat terraform.tfvars | grep -E "^region" | sed "s/region=//g" | sed 's/"//g')
 
-export AWS_REGION=${REGION}
+# export AWS_REGION=${REGION}
 echo "AWS_REGION: ${AWS_REGION}"
 echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
 echo "AWS_SECRET_ACCESS_KEY: ${AWS_SECRET_ACCESS_KEY}"
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+# export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+# export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 
 echo "VPC_ID: ${VPC_ID}"
 echo "REGION: ${REGION}"
 
-# aws configure #set region ${REGION}
+aws configure set region ${REGION}
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 
 echo "Checking VPC exists with ID in AWS: ${VPC_ID}"
 #VPC_ID_OUT=$(aws ec2 describe-vpcs --vpc-ids ${VPC_ID} --query 'Vpcs[0].VpcId' --output=json) 
