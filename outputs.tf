f4bfd473294ac0cd7629a857ac8c20a8498786d6
indirakalagara <indira.kalagara@in.ibm.com>
2022-03-07 10:28:52 +0530

    Fixing output (#7)
    * fix output, enabled flag
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * fix output, enabled flag
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * fix output, enabled flag
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * fix output, enabled flag
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * fix output, enabled flag
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated output
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    * updated output
    
    Signed-off-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
    
    Co-authored-by: IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,42 @@
 output "vpc" {
+
+  depends_on  = [aws_vpc.vpc]
   value = data.aws_vpc.vpc[0]
   
 }
 
 output "vpc_name" {
-  value= data.aws_vpc.vpc[0].tags["Name"]  
+  depends_on  = [aws_vpc.vpc]
+  value= data.aws_vpc.vpc[0].tags["Name"]
 }
 
 output "vpc_id" {
+  depends_on  = [aws_vpc.vpc]
   value = local.vpc_id
   description = "The id for the vpc network"
 }
 
 output "vpc_arn" {
+  depends_on  = [aws_vpc.vpc]
   value       = local.arn
   description = "The arn of the vpc"
 }
 
 output "default_network_acl_id" {
+  depends_on  = [aws_vpc.vpc] 
   value  = local.acl_id
   #value  = ""
   description = "The id for the default network acl"
 }
 
 output "default_security_group_id" {
+  depends_on  = [aws_vpc.vpc]   
   value       = local.security_group_id
   description = "The id of the default security group."
 }
 
 output "default_security_group_arn" {
+  depends_on  = [aws_vpc.vpc]   
   value       = local.security_group_arn
   description = "The arn of the default security group."
 }
